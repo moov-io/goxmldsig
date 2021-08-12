adc2ba511fd18a1ef72abc14f289d107b05c8838
Jose <mfdeveloper508@gmail.com>
2021-08-12 19:01:24 -0400

    disable x509SubjectName

--- sign.go
+++ sign.go
@@ -6,7 +6,6 @@ import (
 	"crypto/rsa"
 	_ "crypto/sha1"
 	_ "crypto/sha256"
-	"crypto/x509"
 	"encoding/base64"
 	"errors"
 	"fmt"
@@ -188,17 +187,6 @@ func (ctx *SigningContext) ConstructSignature(el *etree.Element, enveloped bool)
 		}
 	}
 
-	var roots []*x509.Certificate
-	if cs, ok := ctx.KeyStore.(X509CertificateStore); ok {
-		roots, err = cs.Certificates()
-		if err != nil {
-			return nil, err
-		}
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	rawSignature, err := rsa.SignPKCS1v15(rand.Reader, key, ctx.Hash, digest)
 	if err != nil {
 		return nil, err
@@ -209,23 +197,35 @@ func (ctx *SigningContext) ConstructSignature(el *etree.Element, enveloped bool)
 
 	keyInfo := ctx.createNamespacedElement(sig, KeyInfoTag)
 	x509Data := ctx.createNamespacedElement(keyInfo, X509DataTag)
-	if len(roots) > 0 {
-		for _, root := range roots {
-			x509SubjectName := ctx.createNamespacedElement(x509Data, X509SubjectNameTag)
-			x509SubjectName.SetText(parseDN(root.Subject.String()).String())
-
-			x509IssuerSerial := ctx.createNamespacedElement(x509Data, X509IssuerSerialTag)
-			x509IssuerName := ctx.createNamespacedElement(x509IssuerSerial, X509IssuerNameTag)
-			x509IssuerName.SetText(root.Issuer.String())
-			x509SerialNumber := ctx.createNamespacedElement(x509IssuerSerial, X509SerialNumberTag)
-			x509SerialNumber.SetText(root.SerialNumber.String())
-		}
-	} else {
-		for _, cert := range certs {
-			x509Certificate := ctx.createNamespacedElement(x509Data, X509CertificateTag)
-			x509Certificate.SetText(base64.StdEncoding.EncodeToString(cert))
-		}
+	for _, cert := range certs {
+		x509Certificate := ctx.createNamespacedElement(x509Data, X509CertificateTag)
+		x509Certificate.SetText(base64.StdEncoding.EncodeToString(cert))
 	}
+
+	/*
+		var roots []*x509.Certificate
+		if cs, ok := ctx.KeyStore.(X509CertificateStore); ok {
+			roots, err = cs.Certificates()
+			if err != nil {
+				return nil, err
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+		if len(roots) > 0 {
+			for _, root := range roots {
+				x509SubjectName := ctx.createNamespacedElement(x509Data, X509SubjectNameTag)
+				x509SubjectName.SetText(parseDN(root.Subject.String()).String())
+
+				x509IssuerSerial := ctx.createNamespacedElement(x509Data, X509IssuerSerialTag)
+				x509IssuerName := ctx.createNamespacedElement(x509IssuerSerial, X509IssuerNameTag)
+				x509IssuerName.SetText(root.Issuer.String())
+				x509SerialNumber := ctx.createNamespacedElement(x509IssuerSerial, X509SerialNumberTag)
+				x509SerialNumber.SetText(root.SerialNumber.String())
+			}
+		}
+	*/
 
 	return sig, nil
 }
